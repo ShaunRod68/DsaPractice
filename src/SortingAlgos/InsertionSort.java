dd50e5e894a226530553b268a1548e4f8af611a3
shaunrods <shaunrodrigues68@gmail.com>
2025-07-09 22:59:58 +0530

    nothing

---
+++ src/SortingAlgos/InsertionSort.java
@@ -0,0 +1,9 @@
+package SortingAlgos;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] nums = {3,5,1,7,8,2,12,5};
+        for (int i =0;i< nums.length;i++){
+        }
+    }
+}
--- src/SortingAlgos/SelectionSort.java
+++ src/SortingAlgos/SelectionSort.java
@@ -3,20 +3,16 @@ package SortingAlgos;
 public class SelectionSort {
     public static void main(String[] args) {
         int[] nums = {3,5,1,7,8,2,12,5};
-        int cur = 0;
-        int pos=0;
-        for (int i = 0;i< nums.length;i++){
-            int min = Integer.MAX_VALUE;
-            for (int j = cur ;j< nums.length;j++){
-                if (nums[j]<min){
-                    pos = j;
-                    min=nums[j];
+        for(int i = 0;i< nums.length-1;i++){
+            int misPos = i;
+            for (int j = i+1;j< nums.length;j++){
+                if(nums[misPos]>nums[j]){
+                    misPos=j;
                 }
             }
-            int temp = nums[cur];
-            nums[cur] = nums[pos];
-            nums[pos] = temp;
-            cur++;
+            int temp = nums[i];
+            nums[i]=nums[misPos];
+            nums[misPos]=temp;
         }
         for (int n:nums){
             System.out.print(n+",");
